26880e4ddf071a75619372aadbce973c4d67a333
Bilgehan Gecici <bilgehangecici@gmail.com>
2020-08-11 23:10:45 +0300

    Add files via upload

---
+++ Activity Finder/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Activity from './Activity'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Activity/>
+      </div>
+    );
+  }
+}
+
+export default App;
